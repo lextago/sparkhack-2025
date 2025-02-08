6f26080da0678911e5b99379aefdefd1cdf75fe0
Alexander Tago <alexetago@gmail.com>
2025-02-07 20:29:52 -0600

    finished implementing image upload

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory, url_for
+
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "hello"
+app.config['UPLOADED_PHOTOS_DEST'] = "uploads"
 
-@app.route('/')
-def hello():
-    return render_template("index.html")
+photos = UploadSet("photos", IMAGES)
+configure_uploads(app, photos)
+
+class UploadForm(FlaskForm):
+    photo = FileField(
+        validators=[
+            FileAllowed(photos, "Only images are allowed"),
+            FileRequired("File field should not be empty")
+        ]
+    )
+    submit = SubmitField("Upload")
+
+@app.route('/uploads<filename>')
+def get_file(filename):
+    return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'], filename)
+
+
+@app.route('/', methods=["GET", "POST"])
+def upload_image():
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = photos.save(form.photo.data)
+        file_url = url_for("get_file", filename=filename)
+    else:
+        file_url = None
+
+    return render_template("index.html", form=form, file_url=file_url)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-@app.route("/input", methods=["POST", "GET"])
-def input():
-    if request.method == "POST":
-        user_input = request.form["num"]
-        return f"{int(user_input) * 2}"
-    else:   
-        return render_template("input_test.html")
