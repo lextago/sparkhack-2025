cebe24bf1c48345d8f4e1cf84d5033a21ae63c78
Alexander Tago <alexetago@gmail.com>
2025-02-08 07:46:46 -0600

    edited layout of website

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory, url_for
+from flask import Flask, render_template, request, send_from_directory, url_for, redirect
 
 from flask_uploads import UploadSet, IMAGES, configure_uploads
 from flask_wtf import FlaskForm
@@ -6,6 +6,9 @@ from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
 
 #credit to Red Eyed Coder Club on Youtube for code
+#NOTES: add session cookies to prevent reuploading of same image over and over again
+#add a login to keep track of images and plants
+#add camera input to take photo
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "hello"
@@ -27,18 +30,16 @@ class UploadForm(FlaskForm):
 def get_file(filename):
     return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'], filename)
 
+@app.route('/home')
+def home():
+    return redirect(url_for("index"))
 
 @app.route('/', methods=["GET", "POST"])
-def upload_image():
+def index():
     form = UploadForm()
     if form.validate_on_submit():
         filename = photos.save(form.photo.data)
         file_url = url_for("get_file", filename=filename) #biggggg
     else:
         file_url = None
-
     return render_template("index.html", form=form, file_url=file_url)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
